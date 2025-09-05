5b0a3833c7a07d782e7d46847247602ac8ed71cb
Deadtargaryen <mannyetukudoh@gmail.com>
2025-09-05 02:43:03 +0100

    assignagble max variable assignable to type i16

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 fn main() {
     
-    let mut v1:i32 = 100;
+    let mut v1: i32 = 100;
+
+    let f1: f32 = 3.141;
+
+    let initial: char = 'T';
 
     v1 = v1 + 100;
 
+    let mut v1:i32 = 300;
+
+{
+    v1 = 50;
+    println!("value v1 inside the scope {}", v1);
+}
+
+    println!("value of v1 outside the scope: {}", v1);
+
+
+    println!("i16 max: {}",i16::MAX);
 }
