04c700a677279ee84b2656d721aac0d5986c3f83
Deadtargaryen <mannyetukudoh@gmail.com>
2025-09-05 03:00:14 +0100

    debugging concepts acquired

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ fn main() {
     let a = 100;
     let b = 50;
     let c = 25;
+
+    let mut d: i32 = 0;
+
+    d += 100; // d = d + 100
+
     println!("{}",a+b);
     println!("{}",a-b);
     println!("{}",a*b);
