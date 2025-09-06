93b0947b2ffd497929e00ea734275cc4fb6366e5
Deadtargaryen <mannyetukudoh@gmail.com>
2025-09-06 12:56:37 +0100

    vectors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
 
-    let l:[i32;5] = [5,6,7,8,9];
+    // program.exe 20
+    let mut l:[i32;5] = [5,6,7,8,9];
 
-    println!("{:#?}", l.as_ptr());
-
-    unsafe{
-    let temp = std::ptr::read((l.as_ptr() as isize + (std::mem::size_of::<i32>()*3) as isize) as *const u8);
-    println!("{}", temp);
+    for i in l.into_iter(){
+        // *i =*i + 100 ;
+        println!("{}", i);
     }
-}
+
+
+    println!("{:?}", l.contains(&8)); //gives boolean value true or false
+    }
