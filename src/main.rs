01d577cdadbe8ba995d8a56e1c0dc2a13b1c9bf5
Deadtargaryen <mannyetukudoh@gmail.com>
2025-09-06 02:45:51 +0100

    arrays

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,11 @@
 fn main() {
-    let a = 10;
-    let b = 50;
-    let c = 25;
 
-    let mut d: i32 = 0;
+    let l:[i32;5] = [5,6,7,8,9];
 
-    d += 100; // d = d + 100
-    d -= 50; // d = d - 50
-    d *= 100; // d = d * 100
-    d /= 20; // d = d / 20
+    println!("{:#?}", l.as_ptr());
 
-    //operations
-    println!("{}", a > b);
-    println!("{}", a < b);
-    println!("{}", a == b);
-    println!("{}", a >= b);
-    println!("{}", a % b);
-
-    /*for i in 1..=100{
-            println!("i : {}",i);
-        }
-
-        let mut i = 0;
-        while i<101{
-            println!("{} ",i);
-            i+=2;
-        }
-
-        if a>b{
-            println!("a is greater than b");
-        } else if a==b {
-            println!("a is equal to b");
-        } else{
-            println!("a is lesser than b")
-        }
-    */
-
-    let mut i =0;
-    loop {
-
-        if i==1000{
-            break;
-        }
-
-        println!("{}", i);
-        i+=1;
-
+    unsafe{
+    let temp = std::ptr::read((l.as_ptr() as isize + (std::mem::size_of::<i32>()*3) as isize) as *const u8);
+    println!("{}", temp);
     }
 }
