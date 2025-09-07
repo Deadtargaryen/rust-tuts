5a1aa069430eb4090e16755fcc7968afdfacb962
Deadtargaryen <mannyetukudoh@gmail.com>
2025-09-07 01:46:11 +0100

    vactors, vectors push. empty vector of integers

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 fn main() {
 
     // program.exe 20
-    let mut l:[i32;5] = [5,6,7,8,9];
+    let l:[i32;100] = [0; 100]; //array of 100 integers initialized to 0
 
-    for i in l.into_iter(){
+    let mut v1:Vec<i32> = vec![65, 66, 67, 68, 69]; //vector of integers
+    v1.push(100); //adding element to vector
+
+    let v2:Vec<i32> = Vec::new(); //empty vector of integers
+
+    println!("{:#?}", v1);
+
+    // let t= l.iter().map(|x| x + 200).collect::<Vec<i32>>();
+    // println!("{:#?}", t);
+
+    // for i in l.into_iter(){
         // *i =*i + 100 ;
-        println!("{}", i);
-    }
+    //     println!("{}", i);
+    // }
 
 
-    println!("{:?}", l.contains(&8)); //gives boolean value true or false
+    // println!("{:?}", l.contains(&1)); //gives boolean value true or false
     }
