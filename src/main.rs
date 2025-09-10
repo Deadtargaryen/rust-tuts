f1ce61382bcd7a3fd56b3df2e106a2b3eaeac06c
Deadtargaryen <mannyetukudoh@gmail.com>
2025-09-10 03:11:13 +0100

    ownership cont'd

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-fn testing (name:String) -> String{
-    return name;
+fn testing (name:&mut String){
+    name.push_str(" is the best");
 }
 
 fn main() {
     let mut s = String::from("tech69");
 
-    let s2 = &s;
-    let s3 = &s;
+    let reader1 = &s;
+    let reader2 = &s;
 
-    s = testing(s);
+    let writer1 = &mut s;
+    //let writer2 = &mut s; //cannot borrow as mutable more than once
+
+    //testing(&mut s);
 
     //as long as a value is borrowed and in scope, you cannot change those values
+    //consuming means taking ownership of a value i.e it is a taking a parameter and it is doing any operation to the value
+    println!("{}", writer1);
+    
     println!("{}", s);
 
 }
