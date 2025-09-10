b62290e4018480966482598235392a978f418120
Deadtargaryen <mannyetukudoh@gmail.com>
2025-09-10 02:41:22 +0100

    borrowing 1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 fn main() {
-    let mut a = 6;
-    let b = &a;
+    let mut s = String::from("tech69");
 
-    a += 10;
+    let s2 = &s;
+    let s3 = &s;
 
-    println!("{}", a); 
-    // println!("{}", *b); 
+    s.push_str( "hello");
+
+    //as long as a value is borrowed and in scope, you cannot change those values
+    println!("{}", s);
+    println!("{}", s2);
 
 }
     
