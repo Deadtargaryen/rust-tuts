9b6be6f1e764f6c434baf6d03382eb109e7e8f1e
Deadtargaryen <mannyetukudoh@gmail.com>
2025-09-10 03:36:35 +0100

    reading user input one read_line at a time

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,14 @@
-fn testing () -> String{
-    //dangling reference 
-    let myname = String::from("lenovo");
-    return myname;
-}
-
+use std::io;
 
 fn main() {
-    let mut s = String::from("tech69");
-
-    let reader1 = &s;
-    let reader2 = &s;
-
-    let writer1 = &mut s;
-    //let writer2 = &mut s; //cannot borrow as mutable more than once
-
-    //testing(&mut s);
-
-    //as long as a value is borrowed and in scope, you cannot change those values
-    //consuming means taking ownership of a value i.e it is a taking a parameter and it is doing any operation to the value
-    println!("{}", writer1);
     
-    println!("{}", s);
+    let mut n = String::new();
+
+    println!("Enter a number");
+
+    io::stdin().read_line(&mut n).expect("expected user input");
+
+    println!("you have entered: {}", n.trim().parse::<i32>().unwrap());
 
 }
     
