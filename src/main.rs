82ed250f7e1a91dfa81b8a197d3810aa1176b359
Deadtargaryen <mannyetukudoh@gmail.com>
2025-09-11 23:42:32 +0100

    ownership in fns

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
 
-fn countupto(x:i32) -> i32{
+pub fn countupto(x:i32) -> i32{
     let mut counter = 0;
     for i in 1..x+1{
         counter += i;
     }
 
     // println!("{}", counter);
-    counter
+    return counter;
 }
+
+
+pub fn adder(a:Vec<i32>){
+    println!("{:?}", a);
+}
+
 
 fn main() {
 //call the function
-   let res =  countupto(10);
-    println!("{}", res);
+//    let res =  countupto(10);
+
+// let arr = [5,6,7,8,9];
+// adder(&arr);
+
+
+let v1 = (0..50).collect::<Vec<i32>>();
+adder(v1);
 }
     
