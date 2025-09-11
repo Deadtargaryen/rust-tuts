5ac6b1468269ff11db0ca42912463e53c66f70a5
Deadtargaryen <mannyetukudoh@gmail.com>
2025-09-12 00:25:33 +0100

    de-reference operator

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,19 @@
 
-pub fn countupto(x:i32) -> i32{
-    let mut counter = 0;
-    for i in 1..x+1{
-        counter += i;
+fn main() {
+
+    let mut a = 4;
+
+
+
+    unsafe {
+
+        let p = &a as *const i32;
+
+        println!("pointer value: {:x?}", *p);
+
     }
 
-    // println!("{}", counter);
-    return counter;
-}
 
-
-pub fn adder(a:&Vec<i32>){
-    println!("{:?}", a);
-}
-
-
-fn randongeneration() ->String {
-    let name =  "tech69";
-    return name.to_string();
-}
-
-fn main() {
-//call the function
-//    let res =  countupto(10);
-
-// let arr = [5,6,7,8,9];
-// adder(&arr);
-
-
-    // let v1 = (0..50).collect::<Vec<i32>>();
-    // adder(&v1);
-
-    // println!("{:?}", v1);
-
-
-    println!("{}", randongeneration());
+    println!("{}", a)
 }
     
