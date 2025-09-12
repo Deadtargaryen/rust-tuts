6f4971d13615306ae27ca24989ce63f07c4ffa5a
Deadtargaryen <mannyetukudoh@gmail.com>
2025-09-12 02:48:07 +0100

    ..read len & cap

--- src/main.rs
+++ src/main.rs
@@ -4,34 +4,23 @@ fn main() {
 
     let mut a = 4;
 
-    let mut b = [5,6,7,8];
+    let mut b = vec![5,6,7,8];
 
 
     unsafe {
 
-        let p = &mut b as *mut i32; // or b.as_mut_ptr()
+        let p = &b  as *const Vec<i32>;
 
-        let temp = std::ptr::read((p as usize+12) as *mut i32);
-
-
-        let q = b.as_mut_ptr() as *mut c_void;
-
-        /*let p = &mut a as *mut i32;
-
-        println!("pointer value: {:x?}", p);
-
-        println!("address of a: {:x?}", std::ptr::addr_of!(a));
-
-        // *p = *p + 10; 
-
-
-        std::ptr::write(p as *mut i32, 100);
-
-        let temp = std::ptr::read(p as *const i32);
+        println!("address of a: {:x?}", std::ptr::addr_of!(b));
         
-        println!("{}", temp)*/
+        let q = b.as_mut_ptr();
 
-        println!("{}", temp);
+        //reading len & capacity of vectors
+        let temp = std::ptr::read((p as usize+8) as *mut i32);
+        println!("{:x?}", temp);
+        
+
+
     }
 
 
