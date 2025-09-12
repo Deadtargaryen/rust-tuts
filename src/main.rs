138378570855609de384cc34a91798d74bd2ce7c
Deadtargaryen <mannyetukudoh@gmail.com>
2025-09-12 01:33:12 +0100

    pointer operations...arthmc

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,34 @@ fn main() {
 
     let mut a = 4;
 
+    let mut b = [5,6,7,8];
 
 
     unsafe {
 
-        let p = &mut a as *mut i32;
+        let p = &mut b as *mut i32; // or b.as_mut_ptr()
+
+        let temp = std::ptr::read((p as usize+4) as *mut i32);
+
+        /*let p = &mut a as *mut i32;
 
         println!("pointer value: {:x?}", p);
 
         println!("address of a: {:x?}", std::ptr::addr_of!(a));
 
-        *p = *p + 10; 
+        // *p = *p + 10; 
 
+
+        std::ptr::write(p as *mut i32, 100);
+
+        let temp = std::ptr::read(p as *const i32);
+        
+        println!("{}", temp)*/
+
+        println!("{}", temp);
     }
 
 
-    println!("{}", a)
+    // println!("{}", a)
 }
     
